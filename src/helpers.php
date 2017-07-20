972a7fd8d8bb60a8fafa253a5b1c69b600e6d63c
DDiimmkkaass <ddiimmkkaass@gmail.com>
2017-07-20 11:20:55 +0300

    check if the helper function get_namespace already exists in a project

--- src/helpers.php
+++ src/helpers.php
@@ -13,24 +13,26 @@ if (! function_exists('app_path')) {
     }
 }
 
-/**
- * Get the application namespace.
- *
- * @return string
- *
- * @throws \RuntimeException
- */
-function get_namespace()
-{
-    $composer = json_decode(file_get_contents(base_path('composer.json')), true);
-    
-    foreach ((array) data_get($composer, 'autoload.psr-4') as $namespace => $path) {
-        foreach ((array) $path as $pathChoice) {
-            if (realpath(app_path()) == realpath(base_path().'/'.$pathChoice)) {
-                return $namespace;
+if (! function_exists('get_namespace')) {
+    /**
+     * Get the application namespace.
+     *
+     * @return string
+     *
+     * @throws \RuntimeException
+     */
+    function get_namespace()
+    {
+        $composer = json_decode(file_get_contents(base_path('composer.json')), true);
+        
+        foreach ((array) data_get($composer, 'autoload.psr-4') as $namespace => $path) {
+            foreach ((array) $path as $pathChoice) {
+                if (realpath(app_path()) == realpath(base_path().'/'.$pathChoice)) {
+                    return $namespace;
+                }
             }
         }
+        
+        throw new RuntimeException('Unable to detect application namespace.');
     }
-    
-    throw new RuntimeException('Unable to detect application namespace.');
 }
